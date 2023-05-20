7a8b7de950af1362f424a27d2c5ef213d5d39931
sonukuldeep <sonukuldeep@outlook.com>
2023-05-20 13:23:27 +0530

    conditionals

--- conditionals/src/main.rs
+++ conditionals/src/main.rs
@@ -1,3 +1,25 @@
 fn main() {
-    println!("Hello, world!");
+    if 1 == 2 {
+        println!("Rust is broken");
+    } else {
+        println!("Cool!");
+    }
+
+    let formal = true;
+    let _greeting = if formal {
+        println!("Good evening");
+    } else {
+        println!("Whats up");
+    };
+
+    let x = 1;
+    match x {
+        1 => println!("one"),
+        2 => println!("two"),
+        3 => println!("three"),
+        4 => println!("four"),
+        5 => println!("five"),
+        _ => println!("something else"),
+    }
+
 }
