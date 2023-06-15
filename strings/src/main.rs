88dc16f3db3604fe43dffec4768a92f9b6ceafe5
sonukuldeep <sonukuldeep@outlook.com>
2023-06-15 10:07:05 +0530

    rework

--- strings/src/main.rs
+++
@@ -1,8 +0,0 @@
-fn main() {
-    let text: &str = "Hello\nWorld\n!";
-    println!("First line is: {}", first_line(text));
-}
-
-fn first_line(string: &str) -> &str {
-    string.lines().next().unwrap()
-}
